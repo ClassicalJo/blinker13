7efd7edbc2ec8a5f6daf0570ab9be1087665ed58
ClassicalJo <jb.barrientos@yahoo.com>
2021-08-18 03:22:30 -0300

    ADDED PLAYER CLASS
    Properly extending the Sprite class

--- src/index.js
+++ src/index.js
@@ -8,6 +8,45 @@ bindKeys('ctrl', function () {
     switcheroo()
     setTimeout(() => switcherooOnCooldown = false, 500)
 })
+
+//REFACTOREAR UUUUUU console.log
+let directions = {
+    'left': kontra.Vector(-1, 0),
+    'right': kontra.Vector(1, 0),
+    'up': kontra.Vector(0, -1),
+    'down': kontra.Vector(0, 1),
+    'rightup': kontra.Vector(1, -1).normalize(),
+    'rightdown': kontra.Vector(1, 1).normalize(),
+    'leftdown': kontra.Vector(-1, 1).normalize(),
+    'leftup': kontra.Vector(-1, -1).normalize(),
+}
+
+function getDirectionVector(body) {
+    let vector = kontra.Vector(body.dx, body.dy)
+    if (vector.x === 0 && vector.y === 0) return kontra.Vector(0,-1).normalize()
+    return vector.normalize()
+}
+
+function getDirection(body) {
+    let { x, y } = getDirectionVector(body)
+    let absX = Math.abs(x)
+    let absY = Math.abs(y)
+
+    if (x === NaN && y === NaN) return directions.leftup
+    let thresholdUnit = 0.35
+    let isMono = (Math.max(absX, absY) - Math.min(absX, absY)) > thresholdUnit
+
+    if (isMono) {
+        if (absX > absY) return x < 0 ? directions.left : directions.right
+        else return y < 0 ? directions.up : directions.down
+    }
+    else {
+        let a = x < 0 ? 'left' : 'right'
+        let b = y < 0 ? 'up' : 'down'
+        return directions[a + b]
+    }
+}
+
 const bodies = []
 const playerBodies = [player, shadow]
 
@@ -22,7 +61,10 @@ bodies.push(sprite)
 
 const loop = kontra.GameLoop({
     update: () => {
-        playerBodies.forEach(key => key.update())
+        for (let i = playerBodies.length - 1; i >= 0; i--) {
+            if (playerBodies[i].destroy) playerBodies[i].splice(i, 1)
+            else playerBodies[i].update()
+        }
         bodies.forEach(key => key.update());
     },
     render: () => {
--- src/player.js
+++ src/player.js
@@ -47,152 +47,150 @@ class Combo {
 
 }
 
+class Step {
+    constructor(stepDistance, stepTimeout) {
+        this.distance = stepDistance,
+            this.timeout = stepTimeout
+    }
+}
 class Combo1 extends Combo {
     constructor() {
-        
-        super([10, 15, 25], 500, 4000);
+        let step1 = new Step(20, 500)
+        let step2 = new Step(5, 1000)
+        let step3 = new Step(25, 1000)
+        super([step1, step2, step3], 500, 4000);
         this.attackDistance = 50
     }
     attack(body) {
-        let move = this.steps[this.currentStep]
-        let swordOffsetX = leftRightSwitch(-this.attackDistance, this.attackDistance, 0)
-        let swordOffsetY = upDownSwitch(-this.attackDistance, this.attackDistance, 0)
-        if (Math.abs(swordOffsetX) > 0 && Math.abs(swordOffsetY) > 0) {
-            swordOffsetY /= 1.5
-            swordOffsetX /= 1.5
-        }
-
+        let move = this.steps[this.currentStep].distance
         if (noDirection()) {
-            body.dy = -move
-            let sword = this.craftSword(0, 0)
-            body.addChild(sword)
+            let direction = getDirection(body)
+            if (body.dx !== 0 && body.dy !== 0) {
+                body.dx = direction.x * move
+                body.dy = direction.y * move
+            }
 
+            let sword = this.craftSword(body)
+            body.addChild(sword)
         }
         else {
-            let sword = this.craftSword(swordOffsetX, swordOffsetY)
             body.dx = leftRightSwitch(-move, move, 0)
             body.dy = upDownSwitch(-move, move, 0)
+            let sword = this.craftSword(body)
             body.addChild(sword)
+
         }
-
-
     }
-    craftSword(x, y) {
-        function swordRotation() {
-            let angleMap = {
-                'up': 45,
-                'rightup': 90,
-                'right': 135,
-                'rightdown': 180,
-                'down': 225,
-                'leftdown': 270,
-                'left': 315,
-                'leftup': 360,
-            }
-            //POSSIBLE SHIELD 
-            // let a = leftRightSwitch(90, -90, null)
-            // let b = upDownSwitch(0, 180, null)
-            let a = leftRightSwitch('left', 'right', null) || ''
-            let b = upDownSwitch('up', 'down', null) || ''
-            let direction = a + b
-
-            return degToRad(angleMap[direction])
-
-            // else if (absA > 0) return degToRad(a)
-            // else if (absB > 0) return degToRad(b)
-            // else return 0
-
-        }
+    craftSword(body) {
+        let direction = getDirection(body)
+        let theta = Math.atan2(direction.y, direction.x)
+        let swingOffset = degToRad(135)
+        let offset = { x: -body.width * 4, y: -body.height * 4 }
         return kontra.Sprite({
-            x: 0,
-            y: -75,
-            rotation: swordRotation(),
-            width: 50,
+            x: offset.x * Math.cos(theta + swingOffset),
+            y: offset.y * Math.sin(theta + swingOffset),
+            rotation: theta + swingOffset,
+            width: 100,
             anchor: { x: 0.5, y: 0.5 },
             height: 5,
             color: 'red',
             swing: {
                 should: true,
-                total: 100,
-                accumulated: 0,
-                duration: 50,
-            },
-            rotate: {
-                should: true,
                 total: degToRad(90),
                 accumulated: 0,
-                duration: 10,
+                duration: 100,
             },
             update: function () {
-                if (this.rotate.should) {
-                    let speed = this.rotate.total / this.rotate.duration
+                if (this.swing.should && this.swing.accumulated < this.swing.total) {
+                    let speed = this.swing.total / this.swing.duration
                     this.rotation += speed
-                    this.rotate.accumulated += speed
-                    this.x = -75 * Math.cos(this.rotation)
-                    this.y = -75 * Math.sin(this.rotation)
-                    if (this.rotate.accumulated >= this.rotate.total) this.rotate.should = false
-
+                    this.swing.accumulated += speed
+                    this.x = offset.x * Math.cos(this.rotation)
+                    this.y = offset.y * Math.sin(this.rotation)
                 }
-            }
-
+                else body.removeChild(this)
+            },
         })
     }
 
 
 }
 
-const playerSprite = {
-    x: 200,
-    y: 200,
-    baseAccel: 0.25,
-    speedLimit: 5,
-    radius: 25,
-    attackCooldown: false,
-    combo: null,
-    released: true,
-    attack: function () {
+
+class Player extends kontra.Sprite.class {
+    constructor(x, y, color, name) {
+        super();
+        this.x = x
+        this.y = y
+        this.moveSpeed = 0.25
+        this.speedLimit = 5
+        this.baseSpeed = 2
+        this.width = 25
+        this.height = 25
+        this.anchor = { x: 0.5, y: 0.5 }
+        this.attackCooldown = false
+        this.combo = null
+        this.released = true
+        this.baseColor = color
+        this.color = color
+        this.name = name
+    }
+    attack() {
         this.color = 'red'
         this.released = false
         this.attackCooldown = true
         this.combo.attack(this)
         this.combo.next()
-    },
-    canAttack: function () {
-        return keyPressed('shift') && !this.attackCooldown && this.released
-    },
-    isActive: function () {
-        return status = this.name === activeSprite
-    },
-    reset: function () {
+    }
+    canAttack = () => keyPressed('shift') && !this.attackCooldown && this.released
+    isActive = () => this.name === activeSprite
+    reset() {
         this.combo = null
         setTimeout(() => {
             this.attackCooldown = false
             this.color = this.baseColor
         }, 2000)
-    },
-    cancel: function () {
+    }
+    shouldCancel = () => this.combo !== null && this.combo.canceled
+    cancel() {
         this.combo = null
         this.color = "goldenrod"
-    },
-    shouldCancel: function () {
-        return this.combo !== null && this.combo.canceled
-    },
-    setReady: function (cooldown) {
+    }
+
+
+    setReady(cooldown) {
         setTimeout(() => {
             this.attackCooldown = false
             this.color = 'green'
         }, cooldown)
-    },
-    move: function () {
-        this.ddx = leftRightSwitch(-this.baseAccel, this.baseAccel, 0)
-        this.ddy = upDownSwitch(-this.baseAccel, this.baseAccel, 0)
-        if (Math.abs(this.dx) > this.speedLimit) this.dx *= 0.9
-        if (Math.abs(this.dy) > this.speedLimit) this.dy *= 0.9
-        if (!keyPressed('up') && !keyPressed('down')) this.dy *= 0.98
-        if (!keyPressed('left') && !keyPressed('right')) this.dx *= 0.98
+    }
+    move() {
+        let y = upDownSwitch(-1, 1, 0)
+        let x = leftRightSwitch(-1, 1, 0)
+        
+        if (y !== 0 || x !== 0) {
+            
+            this.rotation = Math.atan2(y, x)
+            console.log(this.rotation)
+        }
+
+        let shouldTurbo = (scalar, minSpeed) => Math.abs(scalar) < minSpeed
+        let shouldSlow = (scalar, maxSpeed) => Math.abs(scalar) > maxSpeed
+
+        if (shouldTurbo(this.dx, this.baseSpeed)) this.dx = this.baseSpeed * x
+        if (shouldTurbo(this.dy, this.baseSpeed)) this.dy = this.baseSpeed * y
+
+        this.ddx = this.moveSpeed * x
+        this.ddy = this.moveSpeed * y
+
+        if (shouldSlow(this.dx, this.speedLimit)) this.dx *= 0.9
+        if (shouldSlow(this.dy, this.speedLimit)) this.dy *= 0.9
+        if (!keyPressed('up') && !keyPressed('down')) this.dy *= 0.95
+        if (!keyPressed('left') && !keyPressed('right')) this.dx *= 0.95
         this.advance()
-    },
-    update: function () {
+    }
+    update() {
+        this.children.forEach(key => key.update())
         if (!this.isActive()) return
         if (this.shouldCancel()) this.cancel()
         if (this.canAttack()) {
@@ -203,28 +201,12 @@ const playerSprite = {
         }
         else if (!keyPressed('shift')) this.released = true
         this.move()
-    },
-    render: function () {
-        this.context.fillStyle = this.color;
-        this.context.beginPath();
-        this.context.arc(0, 0, this.radius, 0, 2 * Math.PI);
-        this.context.fill();
     }
 }
 
-const player = kontra.Sprite({
-    baseColor: 'goldenrod',
-    color: "goldenrod",
-    name: "player",
-    ...playerSprite
-})
+const player = new Player(400, 200, 'goldenrod', 'player')
+const shadow = new Player(200, 200, 'purple', 'shadow')
 
-const shadow = kontra.Sprite({
-    baseColor: "purple",
-    color: "purple",
-    name: 'shadow',
-    ...playerSprite,
-})
 
 function makeLink(origin, dest) {
     return kontra.Sprite({
